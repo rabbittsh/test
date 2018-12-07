089cfcc276c852479578da03cfc8f31069464e3f
tian <tianshihaopsss@163.com>
2018-12-07 10:32:25 +0000

    test

--- pscloud_training/models/subject.py
+++ pscloud_training/models/subject.py
@@ -6,7 +6,7 @@ class TrainingSubject(models.Model):
     _name = 'pscloud.training.subject'
     _description = "科目"
 
-    name = fields.Char(string='名称',size=32，translate=True)
+    name = fields.Char(string='名称')
     person_id = fields.Many2one('res.partner', string='负责人')
     lesson_ids = fields.One2many('pscloud.training.lesson', 'subject_id', string='课程')
     desc = fields.Text(string='描述')
