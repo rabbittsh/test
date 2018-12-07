46772a6a69d63e7099e1acf338fdc486c5eacf54
rabbittsh <45586204+rabbittsh@users.noreply.github.com>
2018-12-07 11:32:06 +0000

    test

--- pscloud_training/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ pscloud_training/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'PS Cloud 培训',
+    'name': 'PS Cloud 培训 课时1',
     'version': '12.0.1.0',
-    'summary': 'PS Cloud 培训',
+    'summary': 'PS Cloud 培训 课时1',
     'author': "www.mypscloud.com",
     'website': 'https://www.mypscloud.com/',
     'category': 'Tools',
